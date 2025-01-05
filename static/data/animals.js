e2c7560b5df1645df198f9486d701fcc64a67833
androsoa3 <10840079-androsoa3@users.noreply.replit.com>
2025-01-05 15:06:22 +0000

    Checkpoint after starting plan

--- static/data/animals.js
+++ static/data/animals.js
@@ -1,10 +1,12 @@
 const animals = [
-    { spanish: "gato", english: "cat", image: "/static/gato.webp" },
-    { spanish: "perro", english: "dog", image: "/static/perro.jpg" },
+    { spanish: "esqueleto", english: "skeleton", image: "/static/esqueleto.webp" },
+    { spanish: "enderman", english: "enderman", image: "/static/enderman.png" },
+    { spanish: "zombie", english: "zombie", image: "/static/zombie.webp" },
+    { spanish: "gallina", english: "chicken", image: "/static/gallina.webp" },
     { spanish: "vaca", english: "cow", image: "/static/vaca.webp" },
     { spanish: "caballo", english: "horse", image: "/static/caballo.jpg" },
     { spanish: "toro", english: "bull", image: "/static/toro.jpg" },
     { spanish: "lobo", english: "wolf", image: "/static/lobo.jpg" },
     { spanish: "loro", english: "parrot", image: "/static/loro.jpg" },
-    { spanish: "oso", english: "bear", image: "/static/oso.jpg" }
+
 ];
