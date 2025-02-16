453d1cdadfd5ff82e5683237c8d0c3f4e1dca315
anibal andrade <59257555+androso@users.noreply.github.com>
2025-02-16 01:51:42 +0000

    Assistant checkpoint: Added Chapter 2 with 19 new words and translations
    Assistant generated file changes:
    - static/data/animals.js: Add Chapter 2 with new words
    
    ---
    
    User prompt:
    
    let's add a new chapter titled: "Chapter 2".  add to this chapter the following words:
    ```
    Gato
    Perro
    Pato
    Oso
    Pez
    Vaca
    Comer
    Correr
    Saltar
    Cantar
    Casa
    Lago
    Selva
    Niña
    Feliz
    Niño
    Árbol
    Jugar
    Rojo
    ```

--- static/data/animals.js
+++ static/data/animals.js
@@ -13,5 +13,30 @@ const chapters = {
             { spanish: "caballo", english: "horse", image: "/static/caballo.jpg" },
             { spanish: "loro", english: "parrot", image: "/static/loro.jpg" },
         ]
+    },
+    capitulo2: {
+        title: "Chapter 2",
+        thumbnail: "/static/gato.webp",
+        words: [
+            { spanish: "Gato", english: "Cat", image: "/static/gato.webp" },
+            { spanish: "Perro", english: "Dog", image: "/static/perro.jpg" },
+            { spanish: "Pato", english: "Duck", image: "/static/delfin.jpg" },
+            { spanish: "Oso", english: "Bear", image: "/static/oso.jpg" },
+            { spanish: "Pez", english: "Fish", image: "/static/pez.jpg" },
+            { spanish: "Vaca", english: "Cow", image: "/static/vaca.webp" },
+            { spanish: "Comer", english: "To eat", image: "/static/gato.webp" },
+            { spanish: "Correr", english: "To run", image: "/static/caballo.jpg" },
+            { spanish: "Saltar", english: "To jump", image: "/static/conejo.webp" },
+            { spanish: "Cantar", english: "To sing", image: "/static/loro.jpg" },
+            { spanish: "Casa", english: "House", image: "/static/serpiente.bmp" },
+            { spanish: "Lago", english: "Lake", image: "/static/pez.jpg" },
+            { spanish: "Selva", english: "Jungle", image: "/static/mono.jpg" },
+            { spanish: "Niña", english: "Girl", image: "/static/mariposa.jpg" },
+            { spanish: "Feliz", english: "Happy", image: "/static/gato.webp" },
+            { spanish: "Niño", english: "Boy", image: "/static/caballo.jpg" },
+            { spanish: "Árbol", english: "Tree", image: "/static/mono.jpg" },
+            { spanish: "Jugar", english: "To play", image: "/static/perro.jpg" },
+            { spanish: "Rojo", english: "Red", image: "/static/toro.jpg" }
+        ]
     }
 };
